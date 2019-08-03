f4ca272abdf6b5649e5fa5fa19f86c84dca1dd4a
[jovanmit] <jovanzmitrovic@gmail.com>
2019-08-03 00:24:21 -0400

    Changed pointers fixed

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1MZRcB8O16sRjKbr70BOfbCqNrZNszPVc'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1MZRcB8O16sRjKbr70BOfbCqNrZNszPVc'
 export_file_name = 'ultrasound_fastai.pkl'
 
 classes = ['gallbladder', 'kidney', 'liver']
